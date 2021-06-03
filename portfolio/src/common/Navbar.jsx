f8959a84fdca1895d2a7e750249e31d10f388c5f
Mayank-MP05 <mayank5pande@gmail.com>
2021-06-03 18:05:36 +0530

    Navlink Errors

--- portfolio/src/common/Navbar.jsx
+++ portfolio/src/common/Navbar.jsx
@@ -18,30 +18,24 @@ function NavbarX() {
   return (
     <>
       <Navbar bg='light' expand='lg'>
-        <Navbar.Brand href='/'>
-          <Link to='/home' className='navbar-brand'>
-            <img src={navLogo} className='navbar-logo' />
-            mayank_mp5
-          </Link>
+        <Navbar.Brand to='/' className='navbar-brand'>
+          <img src={navLogo} className='navbar-logo' />
+          mayank_mp5
         </Navbar.Brand>
         <Navbar.Toggle aria-controls='basic-navbar-nav' className='mx-3' />
         <Navbar.Collapse id='basic-navbar-nav' className='nav-list-container'>
           <Nav className='ml-auto'>
-            <Nav.Link>
-              <Link to='/home' className='nav-link'>
-                Home
-              </Link>
-            </Nav.Link>
-            <Nav.Link>
-              <Link to='/projects' className='nav-link'>
-                Projects
-              </Link>
-            </Nav.Link>
-            <Nav.Link>
-              <Link to='/explore' className='nav-link'>
-                Explore
-              </Link>
-            </Nav.Link>
+            <Link to='/home' className='nav-link'>
+              Home
+            </Link>
+
+            <Link to='/projects' className='nav-link'>
+              Projects
+            </Link>
+
+            <Link to='/explore' className='nav-link'>
+              Explore
+            </Link>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
--- portfolio/src/projects/Projects.jsx
+++ portfolio/src/projects/Projects.jsx
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./projects.css";
 
+//importing icons here
+import CarQuality from "./data/car-quality.png";
+import HulkHuffMan from "./data/hulk-huffman-compression.png";
+import MarathiLetterConv from "./data/Marathi-letter-convnet.png";
+import Molequilizer from "./data/moliquilizer-logo.png";
+import VasooliMoney from "./data/vasooli-money.png";
+
 function Projects() {
+  const [ActiveProject, setActiveProject] = useState(0);
   return (
     <>
       <div className='bg-img'>
         <h3>Projects</h3>
+      </div>
+      <div className='project-navigation'>
+        <img
+          src={CarQuality}
+          className='project-logo'
+          onClick={() => setActiveProject(1)}
+        />
       </div>
     </>
   );
